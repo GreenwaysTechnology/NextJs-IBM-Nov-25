0f2613992e281588b9b50562d82c3a8724b6137e
subugee <sasubramanian_md@hotmail.com>
2025-11-11 12:16:25 +0530

    components app

---
+++ components-app/src/app/counter/page.js
@@ -0,0 +1,8 @@
+import { Counter } from "@/app/counter/components/counter";
+
+export default function CounterPage() {
+    console.log('Counter')
+    return <div>
+        <Counter />
+    </div>
+}
